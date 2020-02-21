a654ccac4c222c138a93d97c7e7dd53154baffec
stephen bilham <stephenbilham@yahoo.com>
2020-02-21 09:37:10 -0800

    game in console complete

--- Javascript/script.js
+++ Javascript/script.js
@@ -249,3 +249,221 @@ interviewQuestionAthlete("mike");
 interviewQuestion("teacher")("mark"); // best no need for storing in variable
 
 */
+
+///////////////
+
+// Bind, call, and apply -  methods
+
+/*
+
+var steve = {
+  name: "steve",
+  age: "26",
+  job: "coder",
+  presentation: function(style, timeOfDay) {
+    if (style === "formal") {
+      console.log(
+        "good " +
+          timeOfDay +
+          ", Ladies and gentlemen! I am " +
+          this.name +
+          ", I am a " +
+          this.job +
+          " I am " +
+          this.age +
+          " years old."
+      );
+    } else {
+      style === "friendly";
+      console.log(
+        "good " +
+          timeOfDay +
+          ", I am " +
+          this.name +
+          ", I am a " +
+          this.job +
+          " I am " +
+          this.age +
+          " years old."
+      );
+    }
+  }
+};
+var emily = {
+  name: "emily",
+  age: 35,
+  job: "designer"
+};
+
+steve.presentation("formal", "morning");
+steve.presentation.call(emily, "friendly", "afternoon"); //call
+steve.presentation.apply(emily, ["friendly", "afternoon"]); //wont work because arg isnt expecting an array
+
+var steveFriendly = steve.presentation.bind(steve, "friendly"); // preset arguements
+steveFriendly("morning");
+steveFriendly("night");
+
+var emilyFormal = steve.presentation.bind(emily, "formal");
+emilyFormal("afternoon");
+
+*/
+
+/*
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+  return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, "20"));
+// A little tricky, Since arrayCalc cant take in full age by itself due to its two elements you need to preset the first one and set the second one to what you want (limit)
+console.log(ages);
+console.log(fullJapan);
+
+*/
+
+/////// PROMPT GAME IN CONSOLE.
+/* 1-7 !!!!!
+
+(function() {
+  function Question(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ": " + this.answers[i]);
+    }
+  };
+
+  Question.prototype.checkAnswer = function(ans) {
+    if (ans === this.correctAnswer) {
+      console.log("correct Answer!");
+    } else {
+      console.log("Wrong answer :(");
+    }
+  };
+
+  questionOne = new Question("how old is Stephen?", [25, 12, 34], 0);
+  questionTwo = new Question("how many dogs does Stephen own?", [1, 2, 5], 1);
+  questionThree = new Question(
+    "What is the speed limit in california highways?",
+    [65, 80, 70],
+    0
+  );
+
+  // console.log(questionOne);
+  // console.log(questionTwo);
+  // console.log(questionThree);
+
+  questionArray = [questionOne, questionTwo, questionThree];
+
+  // console.log(questionArray);
+
+  randomQuestion =
+    questionArray[Math.floor(Math.random() * questionArray.length)];
+
+  randomQuestion.displayQuestion();
+
+  var answer = parseInt(prompt("please select the correct answer"));
+
+  randomQuestion.checkAnswer(answer);
+  // console.log(answer);
+})();
+
+*/
+
+//// EXPERT LEVEL
+
+/*
+
+(function() {
+  function Question(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ": " + this.answers[i]);
+    }
+  };
+
+  Question.prototype.checkAnswer = function(ans, callback) {
+    if (ans === this.correctAnswer) {
+      console.log("correct Answer!");
+      sc = callback(true); // goes to keepScore function if it is true, then it adds a score // this is a closure.
+    } else {
+      console.log("Wrong answer :(");
+      sc = callback(false);
+    }
+    this.displayScore(sc);
+  };
+
+  Question.prototype.displayScore = function(score) {
+    console.log("Your current score is: " + score);
+    console.log("------------------------");
+  };
+
+  questionOne = new Question("how old is Stephen?", [25, 12, 34], 0);
+  questionTwo = new Question("how many dogs does Stephen own?", [1, 2, 5], 1);
+  questionThree = new Question(
+    "What is the speed limit in california highways?",
+    [65, 80, 70],
+    0
+  );
+
+  questionArray = [questionOne, questionTwo, questionThree];
+
+  function score() {
+    var sc = 0;
+    return function(correctAnswer) {
+      if (correctAnswer) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+
+  var keepScore = score();
+
+  function nextQuestion() {
+    randomQuestion =
+      questionArray[Math.floor(Math.random() * questionArray.length)];
+
+    randomQuestion.displayQuestion();
+
+    var answer = prompt("please select the correct answer");
+
+    if (answer !== "exit") {
+      randomQuestion.checkAnswer(parseInt(answer), keepScore);
+      nextQuestion();
+    }
+  }
+
+  nextQuestion();
+})();
+
+*/
+
+//// DONE With GAME
