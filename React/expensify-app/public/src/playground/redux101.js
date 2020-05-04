68065f658b6b1e3851f4a83417611ef96cc9026e
stephen bilham <stephenbilham@yahoo.com>
2020-05-04 09:39:17 -0700

    dispatch dynamic actions redux

--- React/expensify-app/public/src/playground/redux101.js
+++ React/expensify-app/public/src/playground/redux101.js
@@ -1,24 +1,63 @@
 import { createStore } from "redux";
 
 const store = createStore((state = { count: 0 }, action) => {
-  if (action.type === "INCREMENT") {
-    return {
-      count: state.count + 1
-    };
-  } else {
-    return state;
+  switch (action.type) {
+    case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
+      return {
+        count: state.count + incrementBy
+      };
+    case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
+      return {
+        count: state.count - decrementBy
+      };
+    case "SET":
+      return {
+        count: action.count
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 //Actions - nothing more than an object that gets sent to the store
 // increment
 store.dispatch({
+  type: "INCREMENT",
+  incrementBy: 5
+});
+
+store.dispatch({
   type: "INCREMENT"
 });
 
+store.dispatch({
+  type: "RESET"
+});
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: "SET",
+  count: 101
+});
 // i'd like to increment count
 // id like to reset count
-
-console.log(store.getState());
