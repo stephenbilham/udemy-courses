dee9d6e43ce703593f8bb6fa8f5477d7042a1879
stephen bilham <stephenbilham@yahoo.com>
2020-05-12 15:29:17 -0400

    set up and connect r react-redux and

--- React/expensify-app/webpack.config.js
+++ React/expensify-app/webpack.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./public/src/playground/redux-expensify.js",
+  entry: "./public/src/app.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
