d4e2a034fe2ea0bdfca4f2d93f4735a715ad8a27
stephen bilham <stephenbilham@yahoo.com>
2020-09-09 17:53:05 -0700

    working with algos

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//METHOD #1
+// const steps = n => {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
 
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// };
+
+//Recursive Method 2!
+const steps = (n, row = 0, stair = "") => {
+  if (n === row) return;
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+};
+git;
 module.exports = steps;
