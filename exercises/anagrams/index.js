f614b7127c395ac1fe6ab3df59ba896045129be8
stephen bilham <stephenbilham@yahoo.com>
2020-09-09 17:26:17 -0700

    fixing

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,57 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// METHOD #1
+
+// const anagrams = (stringA, stringB) => {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// const buildCharMap = str => {
+//   str = str.toLowerCase();
+//   str = str.replace(/[^a-zA-Z0-9]/g, "");
+
+//   const charMap = {};
+
+//   for (let char of str) {
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+//   return charMap;
+// };
+
+// METHOD #2
+
+const anagrams = (stringA, stringB) => {
+  stringA = removeChar(stringA);
+  stringB = removeChar(stringB);
+
+  return stringA === stringB;
+};
+
+const removeChar = str => {
+  str = str.toLowerCase();
+  str = str.replace(/[^a-zA-Z0-9]/g, "");
+  str = str
+    .split("")
+    .sort()
+    .join("");
+  return str;
+};
 
 module.exports = anagrams;
