9ad78bd32647765ae4a89e9b3c9ac4718037130e
stephen bilham <stephenbilham@yahoo.com>
2020-09-09 17:07:51 -0700

    recursive methods and algos

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// METHOD #1
+// function capitalize(str) {
+//   str = str.split(" ");
+//   const res = [];
+
+//   str.forEach(el => {
+//     res.push(el.charAt(0).toUpperCase() + el.slice(1));
+//   });
+//   return res.join(" ");
+// }
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
 
 module.exports = capitalize;
