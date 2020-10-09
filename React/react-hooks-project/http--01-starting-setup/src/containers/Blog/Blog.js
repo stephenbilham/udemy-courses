878b36ca96c98aee0b6f14a61d59fe4bb0826a20
stephen bilham <stephenbilham@yahoo.com>
2020-10-09 12:10:29 -0700

    addigetting and posting data

--- React/react-hooks-project/http--01-starting-setup/src/containers/Blog/Blog.js
+++ React/react-hooks-project/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
@@ -21,20 +22,31 @@ class Blog extends Component {
           author: "Max"
         };
       });
-
       this.setState({ posts: updatedPosts });
     });
   }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          postSelectedHandler={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
