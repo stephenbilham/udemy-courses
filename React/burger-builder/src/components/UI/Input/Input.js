5387e88ccfd486b4c23c457c5c9dad3806bb01cb
stephen bilham <stephenbilham@yahoo.com>
2020-10-22 10:13:22 -0700

    fixed dynamic component bug

--- React/burger-builder/src/components/UI/Input/Input.js
+++ React/burger-builder/src/components/UI/Input/Input.js
@@ -3,16 +3,16 @@ import React from "react";
 import styles from "./Input.module.css";
 
 const input = props => {
-  console.log(props);
   let inputElement = null;
 
-  switch (props.inputType) {
+  switch (props.elementType) {
     case "input":
       inputElement = (
         <input
           className={styles.InputElement}
           {...props.elementConfig}
           value={props.value}
+          onChange={props.changed}
         />
       );
       break;
@@ -23,10 +23,16 @@ const input = props => {
       break;
     case "select":
       inputElement = (
-        <select className={styles.InputElement} value={props.value}>
-          {props.elementConfig.options.map(option => {
-            return <option value={option.value}>{option.displayValue}</option>;
-          })}
+        <select
+          className={styles.InputElement}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
         </select>
       );
       break;
@@ -49,3 +55,68 @@ const input = props => {
 };
 
 export default input;
+
+// import React from "react";
+
+// import styles from "./Input.module.css";
+
+// const input = props => {
+//   console.log(props);
+//   let inputElement = null;
+
+//   switch (props.elementType) {
+//     case "input":
+//       inputElement = (
+//         <input
+//           className={styles.InputElement}
+//           {...props.elementConfig}
+//           value={props.value}
+//           onChange={props.changed}
+//         />
+//       );
+//       break;
+//     case "textarea":
+//       inputElement = (
+//         <textarea
+//           className={styles.InputElement}
+//           {...props.elementConfig}
+//           value={props.value}
+//           onChange={props.changed}
+//         />
+//       );
+//       break;
+//     case "select":
+//       inputElement = (
+//         <select
+//           className={styles.InputElement}
+//           value={props.value}
+//           onChange={props.changed}
+//         >
+//           {props.elementConfig.options.map(option => (
+//             <option key={option.value} value={option.value}>
+//               {option.displayValue}
+//             </option>
+//           ))}
+//         </select>
+//       );
+//       break;
+//     default:
+//       inputElement = (
+//         <input
+//           className={styles.InputElement}
+//           {...props.elementConfig}
+//           value={props.value}
+//           onChange={props.changed}
+//         />
+//       );
+//   }
+
+//   return (
+//     <div className={styles.Input}>
+//       <label className={styles.Label}>{props.label}</label>
+//       {inputElement}
+//     </div>
+//   );
+// };
+
+// export default input;
