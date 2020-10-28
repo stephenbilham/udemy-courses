2bfd63a8b0a267f6f093d95f9976998eb6dc85d6
stephen bilham <stephenbilham@yahoo.com>
2020-10-28 15:03:00 -0700

    redux refresher store actions reducers listeners providers mapstoprops etc

---
+++ React/redux--01-start/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
---
+++ React/redux--01-start/src/store/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
